3247e23d33b7943379a77cd2e7bccaf4cfb99cb8
Léo Winterhalter <leo.winterhalter@heig-vd.ch>
2025-02-18 14:08:25 +0100

    Firewatch ocean final

--- src/components/TheScene.vue
+++ src/components/TheScene.vue
@@ -3,6 +3,7 @@
 
   import TheCameraRig from './TheCameraRig.vue';
   import '../aframe/tessaletion-square.js';
+  import '../aframe/emit-when-near.js'
 
   const allAssetsLoaded = ref(false);
 </script>
@@ -10,17 +11,23 @@
 <template>
   <a-scene
     stats
-    fog="type: linear; color: #a3d0ed; near: 30; far: 60"
-    background="color: #a3d0ed;"
   >
 
     <a-assets @loaded="allAssetsLoaded = true">
-
+      <img id="firewatch-bg" src="/assets/firewatch-background.png" crossorigin="anonymous" />
     </a-assets>
 
     <template v-if="allAssetsLoaded">
-      <a-entity light="type: ambient; color: #445451"></a-entity>
-      <a-entity light="type: point; intensity: 2" position="0 2 -10"></a-entity>
+
+      <a-sphere position="0 0 -5" emit-when-near="distance: 2"></a-sphere>
+      <a-sky src="#firewatch-bg" radius="5000"></a-sky>
+      
+      <!-- Lumière ambiante générale -->
+      <a-entity light="type: ambient; color: #445451; intensity: 0.7"></a-entity>
+      
+      <!-- Gradient de lumière pour la transition -->
+      <a-entity light="type: hemisphere; color: #ff8533; groundColor: #445451; intensity: 0.5"></a-entity>
+      <a-entity light="type: point; intensity: 0.5; distance: 50" position="0 10 -10"></a-entity>
 
       <a-ocean
         width="100"
@@ -34,16 +41,16 @@
       <a-entity 
         tesseletion-square="
           boxSize: 1; 
-          gap: 0.05; 
+          gap: 0.2; 
           rows: 5; 
           cols: 5;
-          baseHue: 120;
-          baseSaturation: 70;
+          baseHue: 30;
+          baseSaturation: 80;
           minLightness: 30;
           maxLightness: 70"
         position="0 0 -5"
       ></a-entity>
-      </template>
+    </template>
     <TheCameraRig />
 
   </a-scene>
